28d2314c1f6c37a729824230a118e16fed2b07a9
i-eltally <islam.eltally@gmail.com>
2023-03-29 15:25:47 +0200

    status,clone,restore,add,commit

---
+++ 1stPyhton/SyVaTyConcat.py
@@ -0,0 +1,22 @@
+# This is my first Python script
+# print('Hello Python, I love Programming!')
+# print('I love Python!')
+# print(type(3))
+# print(type(3.5252))
+# print(type("Isam Eltally"))
+# print(type([3, 2]))
+# print(type((3, 2)))
+# print(type({"i": "Salma", "j": "Islam"}))
+# print(type(3 == 2))
+# name = "variable lesson P1art 1"
+# print(name)
+# camelCase = "variable lesson P1art 1"
+# print(camelCase)
+# sanke_case = "variable lesson P1art 1"
+# print(sanke_case)
+# reversedWords
+# help("keywords")
+# print("Escape\nSequences\nCharachers")
+# print("Escape\tSequences\tCharachers")
+# print("Escape Sequences Charachers for single quotes\'\'")
+# print(" Escape  Sequences Charachers\r Test to")
